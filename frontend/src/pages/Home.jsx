016fde693de05c8e3cdaf057f4e122ec6760385a
MirzaSameer301 <sameerijaz301@gmail.com>
2025-05-02 11:32:20 +0500

    chat UI with messages send

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,17 @@
 import NoChatSelected from "../components/NoChatSelected";
 import Sidebar from "../components/SideBar";
-// import ChatContainer from "../components/ChatContainer";
+import ChatContainer from "../components/ChatContainer";
+import { useChatStore } from "../store/useChatStore.js";
 
 const Home = () => {
+  const { selectedUser } = useChatStore();
   return (
-    <div className="h-screen bg-base-200">
+    <div className="min-h-screen bg-base-200">
       <div className="flex items-center justify-center pt-20 px-4">
-        <div className="bg-base-100 rounded-lg zshadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+        <div className="bg-base-100 rounded-lg zshadow-cl w-full max-w-6xl h-[calc(105vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
             <Sidebar />
-
-            <NoChatSelected />
+            {selectedUser ? <ChatContainer /> : <NoChatSelected />}
           </div>
         </div>
       </div>
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set,get) => ({
   users: [],
   messages: [],
   selectedUser: null,
@@ -21,7 +21,7 @@ export const useChatStore = create((set) => ({
   getMessages: async (id) => {
     set({ isMessagesLoading: true });
     try {
-      const res = await axiosInstance.get(`/api/messages/${id}`);
+      const res = await axiosInstance.get(`/messages/${id}`);
       set({ messages: res.data });
     } catch (error) {
       console.error("Error fetching messages:", error);
@@ -29,4 +29,17 @@ export const useChatStore = create((set) => ({
       set({ isMessagesLoading: false });
     }
   },
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    try {
+      const res = await axiosInstance.post(
+        `/messages/send/${selectedUser._id}`,
+        messageData
+      );
+      set({ messages: [...messages, res.data] });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
 }));
