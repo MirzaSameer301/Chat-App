bf061c597910e9f7945efc2af85412525a5cf9e2
MirzaSameer301 <sameerijaz301@gmail.com>
2025-05-03 12:50:09 +0500

    changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import messageRoutes from './routes/message.route.js';
 import cookieParser from 'cookie-parser';
 import { app,server } from './lib/socket.js';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 const PORT = process.env.PORT || 5002; 
@@ -20,6 +21,14 @@ app.use(cors({
 
 app.use('/api/auth', authRoutes); 
 app.use('/api/messages', messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {    
   console.log(`Server is running on port ${PORT}`);
